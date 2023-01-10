4805961c941d642363b3d41083d8c8a117337409
Tristan Young <tyoung22@turing.edu>
2023-01-10 15:04:25 -0800

    Show output of js variables

--- variables.js
+++ variables.js
@@ -4,4 +4,10 @@ var age = 24
 var numberofpartners = 3
 var rentPaid = true
 var bedmade = false
+email = "try979@gmail.com"
+username = "codermanstan"
+console.log(email)
+console.log(username)
+console.log(age)
+
 
