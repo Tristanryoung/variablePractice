7c211253773e8823887bb8c348efa80fb08509cf
Tristan Young <tyoung22@turing.edu>
2023-01-11 10:04:52 -0800

    Change all variable names to camel case

--- variables.js
+++ variables.js
@@ -1,9 +1,9 @@
 var email = "tristanryoung@gmail.com"
 var username = "badmanstan"
 var age = 24
-var numberofpartners = 3
+var numberOfPartners = 3
 var rentPaid = true
-var bedmade = false
+var bedMade = false
 email = "try979@gmail.com"
 username = "codermanstan"
 age = 99
